628e0b244f237e9d6b686b35abaf8f9ef6ef23a7
ernst-21 <riverag8504@gmail.com>
2021-10-08 11:10:26 -0400

    fixed issues

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState({ stage: {}, travelInfo: {}, travelIdeas: {}, personalData: {}, rating: {}, negotiation: {negotiationStage: 'New Request', negotiationStageNote: '', negotiationDueDate: Date.now, negotiationMemo: ''}});
+  const [user, setUser] = useState({ stage: {}, travelInfo: {}, travelIdeas: {}, personalData: {}, rating: {}, negotiation: {negotiationStage: 'New Request', negotiationStageNote: '', negotiationDueDate: Date.now, negotiationMemo: '', negotiationStageAction: 'Contact the prospect'}});
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const mongoPSW = process.env.MONGO_PSW
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
-  jwtSecret: process.env.JWT_SECRET || "w\\&r*^HjXh8xgZ2ATC$[\"{?hQ>ny?(8&./$Tf2mC$~5\\HJkU*4",
+  jwtSecret: process.env.JWT_SECRET,
   mongoUri: process.env.MONGODB_URI || `mongodb://ernst:${mongoPSW}@cluster0-shard-00-00.eofmn.mongodb.net:27017,cluster0-shard-00-01.eofmn.mongodb.net:27017,cluster0-shard-00-02.eofmn.mongodb.net:27017/travelers?ssl=true&replicaSet=atlas-10p98v-shard-0&authSource=admin&retryWrites=true&w=majority`
     || process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
